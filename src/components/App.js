0b682829e8f7368ff1f44854f90ff38fb009f389
igornascimento <igor.nascimento@objectedge.com>
2018-10-15 16:03:41 -0300

    Starting project and first components

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CommentBox from './CommentBox';
+import CommentList from './CommentsList';
+
+export default () => {
+  return (
+    <div>
+      <CommentBox />
+      <CommentList />
+    </div>
+  );
+};
