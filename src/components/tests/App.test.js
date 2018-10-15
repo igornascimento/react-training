76f6e5a5d835256ca9bf690171f6d7f6153d66e4
igornascimento <igor.nascimento@objectedge.com>
2018-10-15 16:31:24 -0300

    Fixing test

--- src/components/tests/App.test.js
+++ src/components/tests/App.test.js
@@ -4,7 +4,10 @@ import App from '../App';
 
 it('shows a comment box', () => {
   // this first test just ensure that our component is being loaded
-  const div = document.create('div');
+  const div = document.createElement('div');
   ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtDone(div);
+  
+  console.log(div.innerHTML);
+  
+  ReactDOM.unmountComponentAtNode(div);
 });
